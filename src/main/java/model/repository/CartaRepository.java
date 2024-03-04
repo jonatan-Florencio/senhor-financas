dff1a95786b37d0f9280eade47ea708c2ef939a9
jonatan.florencio <jonatan.florencio@FLE104D04.alunos.sc.senac.br>
2024-03-04 08:52:36 -0300

    Inclusão das classes e arquivos do senhor finanças no meu projeto.

---
+++ src/main/java/model/repository/CartaRepository.java
@@ -0,0 +1,18 @@
+package model.repository;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import model.entity.vemnox1.Carta;
+import model.entity.vemnox1.Partida;
+
+public class CartaRepository implements BaseRepository<Carta> {
+	
+	//TODO porque está com erro? Cenas dos próximos capítulos....
+}
