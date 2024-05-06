711b5edde44a0357eacc2613943697913d602083
Jonatan Florêncio <jonatanflorencio45@gmail.com>
2024-05-06 10:53:20 -0300

    correção de erros

--- src/main/resources/scripts/inserts_vacina.sql
+++ src/main/resources/scripts/inserts_vacina.sql
@@ -1,12 +1,12 @@
-INSERT INTO vacina.pessoa
+INSERT INTO exemplos.pessoa
 (nome, cpf, sexo, data_nascimento, tipo)
 VALUES('Edson Arantes do Nascimento', '01012301010', 'M', '1940-10-23', 1); -- Pesquisador
 
-INSERT INTO vacina.pessoa
+INSERT INTO exemplos.pessoa
 (nome, cpf, sexo, data_nascimento, tipo)
 VALUES('Marcos Evangelista de Morais', '2220002255', 'M', '1970-06-07', 2); -- Voluntário
 
-INSERT INTO vacina.pessoa
+INSERT INTO exemplos.pessoa
 (nome, cpf, sexo, data_nascimento, tipo)
 VALUES('Manoel Francisco dos Santos', '77711122277', 'M', '1933-10-28', 1); -- Pesquisador
 
